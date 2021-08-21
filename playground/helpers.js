f9140c846092f963c11cf4d89355b4c91fe8ffe7
Marko Zabcic <gm.marko@gmail.com>
2021-08-21 23:07:39 +0200

    Add actions and occupation

--- playground/helpers.js
+++ playground/helpers.js
@@ -7,3 +7,8 @@ export function colorFromString(str, s, l) {
   const h = hash % 360;
   return 'hsl('+h+', '+s+'%, '+l+'%)';
 }
+
+
+export function centerTrim(str) {
+  return str.length < 10 ? str : `${str.slice(0, str.length - 6)}...${str.slice(-3, str.length)}`;
+}
