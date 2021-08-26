d123379122b60200630e04aef65928ef403b6a03
Marko Zabcic <gm.marko@gmail.com>
2021-08-26 14:32:37 +0200

    Task1 solution

--- components/wsc-date.js
+++ components/wsc-date.js
@@ -3,9 +3,34 @@ import { LitElement, html } from 'https://cdn.skypack.dev/lit';
 class WscDate extends LitElement {
   createRenderRoot() { return this; }
 
+  static properties = {
+    value: { type: String }
+  }
+
+  get date() {
+    if(!this.value) { return new Date() }
+
+    const timestampOrString = parseInt(this.value, 10) || this.value
+    return new Date(timestampOrString);
+  }
+
+  get year() {
+    return this.date.toLocaleString('en', { year: 'numeric' });
+  }
+
+  get month() {
+    return this.date.toLocaleString('en', { month: 'short' });
+  }
+
+  get day(){
+    return this.date.toLocaleString('en', {day: "2-digit"});
+  }
+
   render() {
     return html`
-      wsc-date not implemented yet!
+      <div class="day">${this.day}</div>
+      <div class="month">${this.month}</div>
+      <div class="year">${this.year}</div>
     `;
   }
 }
